06d3dcf4c0ce8f968e405758e6f297c0e813f88d
bramp <github@bramp.net>
2026-03-26 19:09:59 +0000

    Deploying to gh-pages from @ bramp/prob.js@3fc189c4885bf21431cbea0a746536c25f3a2081 🚀

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
-// Copyright 2016 Google Inc.
+// Copyright 2016-2026 Andrew Brampton
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 // limitations under the License.
 //
 ////////////////////////////////////////////////////////////////////////////////
-(function() {
+(function () {
   'use strict';
   var mt = Random.engines.mt19937().autoSeed();
 
@@ -22,14 +22,14 @@
       showlegend: false,
       margin: {
         t: 20,
-        b: 40
+        b: 40,
       },
-      'xaxis': {
-        range: options.xrange
-      }
+      xaxis: {
+        range: options.xrange,
+      },
     };
     var config = {
-      displayModeBar: false
+      displayModeBar: false,
     };
 
     var nbinsx = options.nbinsx;
@@ -40,24 +40,26 @@
     var bins = [];
     var pdf = [];
     for (var i = 0; i < nbinsx; i++) {
-      x.push(xmin + xrange * i / nbinsx);
+      x.push(xmin + (xrange * i) / nbinsx);
       bins.push(0.0);
     }
-    var data = [{
-      x: x,
-      y: pdf, // (could also be bins)
-      type: 'bar'
-    }];
+    var data = [
+      {
+        x: x,
+        y: pdf, // (could also be bins)
+        type: 'bar',
+      },
+    ];
 
     var count = 0;
 
-    var drawMore = function() {
+    var drawMore = function () {
       var i;
       var iterations = count * 1.1 + 1; // Allow for a slow ramp up
       for (i = 0; i < iterations; i++) {
         var j = r(mt);
         if (j >= xmin && j <= xmax) {
-          bins[ Math.floor((j - xmin) * nbinsx / xrange) ]++;
+          bins[Math.floor(((j - xmin) * nbinsx) / xrange)]++;
         }
         count++;
       }
@@ -79,28 +81,34 @@
     setTimeout(drawMore, 0);
   }
 
-  drawHistogram('normal', Prob.normal(), {  // μ = 0, σ = 1.0
+  drawHistogram('normal', Prob.normal(), {
+    // μ = 0, σ = 1.0
     nbinsx: 120,
-    xrange: [-3, 3]
+    xrange: [-3, 3],
   });
-  drawHistogram('uniform', Prob.uniform(), {  // min = 0, max = 1.0
+  drawHistogram('uniform', Prob.uniform(), {
+    // min = 0, max = 1.0
     nbinsx: 100,
-    xrange: [0, 1]
+    xrange: [0, 1],
   });
-  drawHistogram('exponential', Prob.exponential(), {  // λ = 1.0
+  drawHistogram('exponential', Prob.exponential(), {
+    // λ = 1.0
     nbinsx: 120,
-    xrange: [0, 5]
+    xrange: [0, 5],
   });
-  drawHistogram('lognormal', Prob.lognormal(), { // μ = 0, σ = 1.0
+  drawHistogram('lognormal', Prob.lognormal(), {
+    // μ = 0, σ = 1.0
     nbinsx: 120,
-    xrange: [0, 5]
+    xrange: [0, 5],
   });
-  drawHistogram('poisson', Prob.poisson(4), { // λ = 1.0
+  drawHistogram('poisson', Prob.poisson(4), {
+    // λ = 1.0
     nbinsx: 16,
-    xrange: [0, 16]
+    xrange: [0, 16],
   });
-  drawHistogram('zipf', Prob.zipf(), { // s = 1, N = 100
+  drawHistogram('zipf', Prob.zipf(), {
+    // s = 1, N = 100
     nbinsx: 101,
-    xrange: [0, 101]
+    xrange: [0, 101],
   });
-}());
+})();
